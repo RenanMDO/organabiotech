d4de0b0ff3c60cb9c638ee7f855d282f1b39bac1
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-05-11 14:45:36 -0300

    Chenged email from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -47,7 +47,7 @@ export function Footer() {
             <FooterLinkItems>
               <FooterLinkTitle> Contato </FooterLinkTitle>
               <FooterLink href='https://wa.me/5547984970998?text=Olá%20Guilherme%20te%20encontrei%20pelo%20site,%20gostaria%20de%20conhecer%20melhor%20a%20Organa!' target='_blank'>WhatsApp</FooterLink>
-              <FooterLink href="mailto:contato@organabiotech.com.br" target='_blank'>Email</FooterLink>
+              <FooterLink href="mailto:guilherme@organabiotech.com.br" target='_blank'>Email</FooterLink>
               <FooterLink href='tel:+5547984970998' target='_blank'>Telefone</FooterLink>
               <FooterLink href="https://www.google.com/maps/place/Organa+Biotech+Solu%C3%A7%C3%B5es+Ambientais/@-26.2515686,-48.8777027,17z/data=!3m1!4b1!4m5!3m4!1s0x94deb199fc35c2df:0xfc029aac9ad76b66!8m2!3d-26.2517356!4d-48.8755097" target='_blank'>Endereço</FooterLink>
             </FooterLinkItems>
