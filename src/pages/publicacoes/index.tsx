762957cdcafc7a0fad92e894997b6c9715b3884e
RenanMDO <renan.montenegro.oliveira@gmail.com>
2026-01-16 23:30:41 +0100

    New Publicacoes Page

---
+++ src/pages/publicacoes/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Container } from "../../components/Publicacoes";
+
+export default function Signin() {
+  return (
+    <>
+      <Head>
+        <title>Publicações | Organa Biotech</title>
+        <meta
+          name="description"
+          content="Publicações"
+        />
+      </Head>
+      <Container>
+        
+      </Container>
+    </>
+  )
+}
